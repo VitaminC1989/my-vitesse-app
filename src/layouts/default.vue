3407c49c53d7a5cfb03aa368333765b10c81dc8c
VitaminC1989 <1741253832@qq.com>
2022-09-07 16:05:50 +0800

    feat: 修改默认布局

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,9 +1,12 @@
 <template>
   <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
     <RouterView />
-    <Footer />
-    <div class="mt-5 mx-auto text-center opacity-75 dark:opacity-50 text-sm">
-      [Default Layout]
+
+    <div class="fixed bottom-0 left-1/2 translate-x--1/2">
+      <Footer />
+      <div class="mt-5 mx-auto text-center opacity-75 dark:opacity-50 text-sm">
+        [Default Layout]
+      </div>
     </div>
   </main>
 </template>
