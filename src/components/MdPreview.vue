e127d5ff18101381bdf048df1e8e1c44b705dbb7
VitaminC1989 <1741253832@qq.com>
2022-09-19 16:34:15 +0800

    feat: markdown预览组件

---
+++ src/components/MdPreview.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+// 预览组件以及样式
+import VMdPreview from '@kangc/v-md-editor/lib/preview'
+import '@kangc/v-md-editor/lib/style/preview.css'
+// VuePress主题以及样式（这里也可以选择github主题）
+import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js'
+import '@kangc/v-md-editor/lib/theme/style/vuepress.css'
+
+// Prism
+import Prism from 'prismjs'
+// 代码高亮
+import 'prismjs/components/prism-json'
+
+const props = defineProps<{
+  content: string
+}>()
+
+// 选择使用主题
+VMdPreview.use(vuepressTheme, {
+  Prism,
+})
+</script>
+
+<template>
+  <VMdPreview :text="props.content" />
+</template>
